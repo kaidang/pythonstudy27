4ce368c6f6add45d1560e87a3df92f55981eea9e
xzzdq@163.com <jamesathome>
2016-03-26 20:53:19 +0800

    addclass

---
+++ superclass.py
@@ -0,0 +1,17 @@
+import bs4
+print bs4
+class Filter:
+    def init(self):
+        self.blacked = []
+f = Filter()
+f.init()
+f.blacked = [1,'ok']
+print f.blacked
+x = range(1,101)
+print x
+i = 0
+print len(x)
+while i < len(x):
+    print x[i]
+    i+=1
+
