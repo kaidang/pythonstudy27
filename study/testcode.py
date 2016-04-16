e975bea4ab95d7a3d482fd0066fe9e2a4c6981f1
zdq <zdqown@139.com>
2016-04-16 19:08:01 +0800

    revised 20160413

--- study/testcode.py
+++ study/testcode.py
@@ -1,25 +1,13 @@
 # -*- coding: utf-8 -*-
 
-k = "B5f10******b28******e5****************9d221"
-url = r"http://api.map.baidu.com/direction/v1?mode=driving&origin=上地五街&destination=北京大学&origin_region=北京&destination_region=北京&output=json&ak=" + ak
-import urllib2
-import json
-# import httplib2
+result= [7,8]
 
-def getContent(url):
-    req = urllib2.Request(url)
-    req.set_proxy("siwebproxy01.apac.nokia.com:8080", "http")
-    res = urllib2.urlopen(req)
-    content = res.read()
-    return content
+for kk in zip([0] + result, result + [0]):
+    print kk
 
+print kk
 
-if __name__ == '__main__':
-#     httplib2.urllib.parse.unquote('\u6ce8\u91ca')
-    content = getContent(url)
-    cc = content.decode("unicode_escape")
-    ccc = cc.encode("UTF-8")
-    print ccc
+g= [sum(i) for i in zip([0] + result, result + [0])]
 
-
+print g
 
